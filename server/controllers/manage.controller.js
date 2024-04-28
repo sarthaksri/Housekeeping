db643e6f3b0fabe71d79ce86ae087fd0b61ac1f4
sarthaksri <sarthaksri017@gmail.com>
2024-04-28 16:07:40 +0530

    ss

--- server/controllers/manage.controller.js
+++ server/controllers/manage.controller.js
@@ -1,12 +1,13 @@
 const User = require('../schema/user.js');
+const Request = require('../schema/request.js');
 const asyncHandler = require('express-async-handler');
 
 exports.registerStudent = asyncHandler(async(req,res) =>{
     try{
-        const {rollno, roomno, floor} = req.body;
+        const {rollno, roomno, floor, hostel} = req.body;
         const updatedStudent = await User.findOneAndUpdate(
             { rollno: rollno },
-            { $set: { roomno: roomno, floor: floor } },
+            { $set: { roomno: roomno, floor: floor, hostel : hostel } },
             { new: true }
           );
       
@@ -24,8 +25,18 @@ exports.registerStudent = asyncHandler(async(req,res) =>{
 
 exports.cleanRequest = asyncHandler(async(req,res) =>{
   try{
-     const {reqDate, reqTime} = req.body;
-     
+     const {reqDate, reqTime, roomno, rollno, active} = req.body;
+     const request = await Request.create({
+          rollno: rollno,
+          roomno: roomno,
+          time: reqTime,
+          date: reqDate,
+          active : active
+      });
+
+      return res.status(200).json({
+        success: true
+    });
     }
   catch(error){
       console.log(error);
